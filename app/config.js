80bf30f9360a9dd4b865ddefbfc9e63877414b10
yasmine <taybi.yasmine42@gmail.com>
2026-06-18 16:22:01 +0200

    Fix: add config.js to public repo

---
+++ app/config.js
@@ -0,0 +1,7 @@
+window.OOM_CONFIG = {
+  APPWRITE_ENDPOINT: 'https://cloud.appwrite.io/v1',
+  APPWRITE_PROJECT: '6a1041f50005ea891757',
+  DB_ID: 'ou_on_mange',
+  COL_USERS: 'users',
+  COL_RESTAURANTS: 'restaurants'
+};
